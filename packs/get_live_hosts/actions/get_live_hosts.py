52226273d6230b5ceb648811e7d3d534582451cd
aamir <raza.aamir01@gmail.com>
2015-01-25 12:26:43 -0500

    live hosts

---
+++ packs/get_live_hosts/actions/get_live_hosts.py
@@ -0,0 +1,26 @@
+import nmap
+from st2actions.runners.pythonrunner import Action
+
+
+
+class check_live_hosts(Action):  
+
+  def run(self, network):
+    host_list = []
+    res = nmap.PortScanner()
+    print "Starting Network Discovery...."
+    lh = res.scan(network, arguments='-sP -n')
+    print "Number of Live Hosts on network : ",lh['nmap']['scanstats']['uphosts']
+    print "Live Hosts : "
+    for ip in res.all_hosts():
+      host_list.append(ip)
+    return host_list
+ 
+
+
+if __name__ == "__main__":
+  check_up = check_live_hosts()
+  check_up.run()
+
+
+
