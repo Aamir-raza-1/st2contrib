c4d4e6e21c90ee3b8eedb5641706e0d4b244b747
aamir <raza.aamir01@gmail.com>
2015-01-28 14:36:10 -0500

    parameter name change

--- packs/linux/actions/get_live_hosts.py
+++ packs/linux/actions/get_live_hosts.py
@@ -5,22 +5,11 @@ from st2actions.runners.pythonrunner import Action
 
 class CheckLiveHosts(Action):  
 
-  def run(self, network):
+  def run(self, subnet):
     host_list = []
     res = nmap.PortScanner()
-    print "Starting Network Discovery...."
     lh = res.scan(network, arguments='-sP -n')
-    print "Number of Live Hosts on network : ",lh['nmap']['scanstats']['uphosts']
-    print "Live Hosts : "
     for ip in res.all_hosts():
       host_list.append(ip)
     return host_list
- 
-
-
-if __name__ == "__main__":
-  check_up = check_live_hosts()
-  check_up.run()
-
-
 
